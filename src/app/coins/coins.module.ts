36d6288205c30d901a8333ffd71b7e20c4a49de6
Blake Carroll <bacarrol@usc.edu>
2018-10-21 13:14:20 -0700

    finished coins module

--- src/app/coins/coins.module.ts
+++ src/app/coins/coins.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 // NgRx
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-// reducers and effects
+// Reducers and Effects
 import { reducers, effects } from './store';
-
-
-import { CoinsComponent } from './containers/coins/coins.component';
-//
-
+// Material
+import { MaterialModule } from '../material';
+// Routing
+import { CoinsRoutingModule } from './coins-routing.module';
+// Containers
+import * as fromContainers from './containers';
+// components
+import * as fromComponents from './components';
 
 @NgModule({
   imports: [
@@ -19,7 +22,7 @@ import { CoinsComponent } from './containers/coins/coins.component';
     StoreModule.forFeature('coins', reducers),
     EffectsModule.forFeature(effects),
   ],
-  declarations: [...fromContainers.containers, ...fromComponents.components, CoinsComponent],
+  declarations: [...fromContainers.containers, ...fromComponents.components],
   exports: [...fromContainers.containers, ...fromComponents.components],
 })
 export class CoinsModule { }
